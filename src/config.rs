623fdf784f91bf37057124eaffd594cf33759d48
Ashley Wulber <ashley.wulber@pm.me>
2023-06-26 17:34:48 -0400

    update nix flake

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,2 @@
 pub const APP_ID: &str = "com.system76.CosmicAppLibrary";
 pub const VERSION: &str = "0.1.0";
-
-pub fn profile() -> &'static str {
-    std::env!("OUT_DIR")
-        .split(std::path::MAIN_SEPARATOR)
-        .nth_back(3)
-        .unwrap_or("unknown")
-}
