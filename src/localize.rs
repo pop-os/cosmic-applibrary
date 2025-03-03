c58a3669ee1d9198be17c0b4bb468fb41da5986b
Ashley Wulber <ashley@system76.com>
2025-03-03 10:36:12 -0500

    fix license references

--- src/localize.rs
+++ src/localize.rs
@@ -1,3 +1,6 @@
+// Copyright 2023 System76 <info@system76.com>
+// SPDX-License-Identifier: GPL-3.0-only
+
 use i18n_embed::{
     fluent::{fluent_language_loader, FluentLanguageLoader},
     DefaultLocalizer, LanguageLoader, Localizer,
